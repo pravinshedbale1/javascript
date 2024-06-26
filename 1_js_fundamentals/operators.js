f967acef294f1c318f13055d16b60793d417d15b
pravinshedbale1 <pravin.shedbale@outlook.com>
2024-06-26 09:52:28 +0530

    js fundamentals

---
+++ 1_js_fundamentals/operators.js
@@ -0,0 +1,9 @@
+const now = 2025;
+const pravinsYob = 1999;
+
+console.log("Pravins age", now - pravinsYob);
+
+const firstName = "Pravin";
+const lastName = "Shedbale";
+
+console.log("Full Name ", firstName + " " + lastName);
