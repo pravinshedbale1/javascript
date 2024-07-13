738a35877519d36cac568ee4072a5da38d21de62
pravinshedbale1 <pravin.shedbale@outlook.com>
2024-07-13 14:40:13 +0530

    arrays

--- 1_js_fundamentals/arrays.js
+++ 1_js_fundamentals/arrays.js
@@ -7,3 +7,27 @@ console.log(years);
 
 console.log(typeof friends);
 console.log(typeof years);
+
+// Add elements
+
+//At the end
+friends.push("Rishab");
+console.log(friends);
+
+//At the begining
+friends.unshift("Himanshu");
+console.log(friends);
+
+// Remove Elements
+
+//From the end
+friends.pop();
+console.log(friends);
+
+//From the begining
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf("Pravin"));
+
+console.log(friends.includes("Deepak"));
